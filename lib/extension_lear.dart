a2d87c574ad57cc4d34da0d9fdbf69248f30f41d
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-08-07 02:06:57 -0700

    extension extestclass on testclass

--- lib/extension_lear.dart
+++ lib/extension_lear.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class extension_class extends StatefulWidget {
@@ -20,9 +19,13 @@ class _extension_classState extends State<extension_class> {
             Center(
               child: OutlinedButton(
                 onPressed: () {
-                String s='karet harf nadareh';
-                s.getextensionstringme(s);
+                  // String s = 'karet harf nadareh';
+                  // s.getextensionstringme(s);
 
+                  //estefadeh az class extension jadid
+                  var clsss = testclass();
+                  clsss.getasli(
+                      'miladeshonam ba ghodrat az dare poshtiyeh class miyam to');
                 },
                 child: const Text(
                   'data',
@@ -34,21 +37,34 @@ class _extension_classState extends State<extension_class> {
       ),
     );
   }
-
 }
 //Extension chiyeh? salon arashgari zana ke mohashono Extension mikonan
 //inam hamon karo mikoneh shoma mitonid be class ha ye serii ghabeliyat
 // ba Extension ijad konid va on kari ke mikhaid ro vasaton anjam bede
-
-
 
 //mikha be class String ye method shakhsi bedam
 
 //vase in kar
 extension StringExtensionsas on String {
   //mikham ye extension ro be esm StringExtension = > be mishe on => va class String bechasbonam
-  void getextensionstringme(String s){
+  void getextensionstringme(String s) {
     print(s);
   }
 }
 
+//masalan ma ye class misazim va ye ghabeliyat besh midim
+class testclass {
+  void gettesttext() {
+    print('object');
+  }
+}
+
+//ye ghabeliyat besh midim ba extension
+
+extension extestclass on testclass {
+  void getasli(String s) {
+    print(s);
+  }
+}
+
+//alan 2ta ghabeliyat dareh ke yekish azash jodas
