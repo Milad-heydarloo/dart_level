e7f8a54f85fca9c1ab6a8758d8eca42bc0d711fe
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-08-07 05:12:17 -0700

    factory Car.FactoryBuildChine

--- lib/namedconstructor.dart
+++ lib/namedconstructor.dart
@@ -19,8 +19,10 @@ class _namedconsState extends State<namedcons> {
             Center(
               child: OutlinedButton(
                 onPressed: () {
-                 Car.withoutABS('Iran Khodro ', ' Peykan ', ' 1357 ');
-
+                  Car.withoutABS('Iran Khodro ', ' Peykan ', ' 1357 ');
+                  //parametr price avaliyeh
+                  var cars =Car.FactoryBuildChine(125.000,'hundi', 'Terili', '1995', true);
+                  print(cars.company! + cars.model!);
                 },
                 child: const Text(
                   'data',
@@ -34,8 +36,8 @@ class _namedconsState extends State<namedcons> {
   }
 }
 
-  //age bekhaii az har object ye model motefvet besazi mitoni tavasot named constructor
-  // in karo bokoni
+//age bekhaii az har object ye model motefvet besazi mitoni tavasot named constructor
+// in karo bokoni
 class Car {
   String? company;
   String? model;
@@ -50,9 +52,34 @@ class Car {
   //mesal man mikham ye serii az mashinam bedoneh ABS sakhteh beshan
   Car.withoutABS(this.company, this.model, this.year) {
     this.hasABS = false;
-    print(company!+model!+year!+' va ABS => '+hasABS.toString()+' yani nadareh ');
-
+    print(company! +
+        model! +
+        year! +
+        ' va ABS => ' +
+        hasABS.toString() +
+        ' yani nadareh ');
   }
 
   //Factory Constructor
+
+//hala shoma mikhai be jaye iran vase EU oropa mashin besazi
+//inja be Factory Constructor niyaz darii ta ye mashin vase oropa vasat besazeh
+//Factory Constructor karesh ineh ke ye mohasebehii anjam bede va ye object dige b ma bargardoneh
+  factory Car.FactoryBuildChine(
+      //to in constructor ma ye parametr vorodi be esm
+      //price ham mikhaim vase in to tarif mashin besh gheymat bedim
+      double price,
+      String company,
+      String model,
+      String year,
+      bool hasABS) {
+    //inja mitonam begam ye parametr be in model objectam bede
+    return Car(
+      //inja mitoni az parametr vorodi estefadeh koni hamon price
+      ' ' + company + ' Chin '+' price '+price!.toString(),
+      ' ' + model + ' Chin ',
+      ' ' + year + ' Chin ',
+      true,
+    );
+  }
 }
