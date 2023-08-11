a705254129e04d1910f5b8c29f906258e2adc15c
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-08-10 23:42:35 -0700

    to inyeh mamashon

--- lib/JSON_OBJECT_ARRAY.dart
+++ lib/JSON_OBJECT_ARRAY.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-
+import 'package:flutter/cupertino.dart';
+import 'package:lottie/lottie.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -19,8 +20,10 @@ class _Json_Object_arrayState extends State<Json_Object_array> {
     return SafeArea(
       child: SingleChildScrollView(
         child: Column(
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+
             Text('$txt'),
             Text('$txt2'),
             Center(
@@ -33,6 +36,14 @@ class _Json_Object_arrayState extends State<Json_Object_array> {
                 ),
               ),
             ),
+            Container(
+              height: 400,
+              width: 400,
+              child: Lottie.network(
+
+                  'https://assets-v2.lottiefiles.com/a/053201ee-116e-11ee-9953-db82c80cd2e4/VVOLWkKqWc.json'),
+            ),
+
           ],
         ),
       ),
@@ -53,6 +64,29 @@ class _Json_Object_arrayState extends State<Json_Object_array> {
       //method paiin az address geo ke mishe mogheyat joghrafiyaii ro migireh ke tosh
       // ye tool va arze joghrafiyaiiye
       txt2 = jsonDecode(response.body)['address']['geo']['lat'];
+
+      _get_USERS();
+   
+      
     });
+  }
+  
+  
+  void _get_USERS()async{
+    var uri=Uri.parse('https://jsonplaceholder.typicode.com/users');
+    // https://jsonplaceholder.typicode.com/users JSON ARRAY IN USERS
+
+    Response response=await get(uri);
+    //in chon ye array hast ma az andis 0 rom ke mishe khoneh aval araye estefadeh mikonim ta
+    //esm user ro az khoneh 0 ke mishe aval begirim
+    String website=jsonDecode(response.body)[0]['website'];
+
+    //gerftan esm user ha hamashon dast bezan be mamashon
+    for(int i=0;i<10;i++){
+      var users_name=jsonDecode(response.body)[i]['name'];
+      print(users_name+'  ');
+    }
+    
+    
   }
 }
